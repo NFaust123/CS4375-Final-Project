3ced461e188fe9ade1a25e2c1460044f4e001d0e
NFaust123 <166555569+NFaust123@users.noreply.github.com>
2024-04-29 01:22:36 -0500

    Add files via upload

--- Decision Tree.py
+++ Decision Tree.py
@@ -236,6 +236,18 @@ print(classification_report(y_test, test_prediction))
 fpr, tpr, thresholds = metrics.roc_curve(y_test, test_prediction)
 print(metrics.auc(fpr, tpr))
 
+# plot the ROC curve
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='blue', lw=2, label='ROC curve (area = %0.2f)' % metrics.auc(fpr, tpr))
+plt.plot([0, 1], [0, 1], color='gray', linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc='lower right')
+plt.show()
+
 # confusion matrix before parameter tuning
 cm = confusion_matrix(y_test, test_prediction)
 sns.heatmap(cm,
